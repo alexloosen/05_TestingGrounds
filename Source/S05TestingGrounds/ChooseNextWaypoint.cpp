76ce29b186a27a487922f7d32f96b1b160778040
Alexander Loosen <alexander.loosen@gmx.de>
2018-07-22 19:26:36 +0200

    remove dependencies of ThirdPersonCharacter to PatrollingGuard.h and created PatrolRoute.h/cpp so other actors can move have AI aswell

--- Source/S05TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05TestingGrounds/ChooseNextWaypoint.cpp
@@ -3,15 +3,23 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" // TODO Remove coupling
+//#include "PatrollingGuard.h" // TODO Remove coupling
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// TODO Protect against no patrol route component
+
 	// Get Patrol Points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute))
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
 
 	if (PatrolPoints.Num() > 0)
 	{
@@ -23,6 +31,10 @@ EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& Own
 		// Cycle Index
 		auto NextIndex = (Index + 1) % PatrolPoints.Num();
 		BlackbordComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	}
+	else
+	{
+		return EBTNodeResult::Failed;
 	}
 
 	return EBTNodeResult::Succeeded;
